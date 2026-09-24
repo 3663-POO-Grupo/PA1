3cc041d8d06f01a712170d0e6bd1c5c9d802ba74
Jvaope <jesus@vaope.com>
2026-09-23 19:27:44 -0500

    Fix: Añade comentario sobre la reubicación del código de Dante Zosimo Alanya Molina en SistemaControl.java

--- src/SistemaControl.java
+++ src/SistemaControl.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+// Se traslado al archivo correcto el codigo de Dante Zosimo Alanya Molina
 public class SistemaControl {
 
     public static void main(String[] args) {
